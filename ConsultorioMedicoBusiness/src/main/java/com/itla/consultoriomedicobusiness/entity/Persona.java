3f27e8e3a1e2a7c5f9bd5b527a762203c9b6dc0e
jldelacruzortega <joseluisdelacruzortega@gmail.com>
2017-08-14 00:27:54 +0400

    Practica 4

---
+++ ConsultorioMedicoBusiness/src/main/java/com/itla/consultoriomedicobusiness/entity/Persona.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itla.consultoriomedicobusiness.entity;
+
+/**
+ *
+ * @author JoseLuis
+ */
+public abstract class Persona {
+    
+    protected Long id;
+    protected String nombre;
+    protected String apellido;
+    protected String sexo;
+    protected String direccion;
+    protected String correo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+}
